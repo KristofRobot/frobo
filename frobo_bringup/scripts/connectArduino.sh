ac3423b76e3684c822bb0c6a5af9df45577d24e2
Kristof Robot <krirobo@gmail.com>
2014-03-08 21:27:31 +0100

    connectArduino: usin port naming

--- frobo_bringup/scripts/connectArduino.sh
+++
@@ -1,3 +0,0 @@
-#picocom -c -b 115200 /dev/ttyS1
-picocom -c -b 115200 /dev/ttyACM0
-#To exit picocom, use CNTL-A followed by CNTL-X
---
+++ frobo_bringup/scripts/connectArduinoMotor.sh
@@ -0,0 +1,2 @@
+echo "To exit picocom, use CNTL-A followed by CNTL-X"
+picocom -c -b 115200 /dev/arduino_motor
