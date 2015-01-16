76c5602dfafc3d1689ad5efd0479a791507e2fd7
Kristof Robot <krirobo@gmail.com>
2015-01-16 21:32:00 +0100

    frobo_bringup: adding commands to laserStart script to start/stop motor

--- frobo_bringup/scripts/laserStart.sh
+++ frobo_bringup/scripts/laserStart.sh
@@ -1,1 +1,13 @@
+#Set port
+stty -F /dev/lidar cs8 115200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+
+#Start motor
+echo 'MotorOn' > /dev/lidar
+#Start sending raw data
+echo 'ShowRaw' > /dev/lidar
+
+#Start laser driver
 rosrun xv_11_laser_driver neato_laser_publisher _port:=/dev/lidar _firmware_version:=2 _frame_id:=/lidar_link
+
+#Stop motor when above process ends
+echo 'MotorOff' > /dev/lidar
