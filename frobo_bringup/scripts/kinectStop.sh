824059b1c2ca3bc1b7dcbeddbad96cdf5a24193c
KristofRobot <KristofRobot@users.noreply.github.com>
2015-03-21 13:34:21 +0100

    frobo-bringup: updating kinectStop.sh script for new USB hub

--- frobo_bringup/scripts/kinectStop.sh
+++ frobo_bringup/scripts/kinectStop.sh
@@ -1,6 +1,6 @@
 echo "Turning off Kinect..."
-echo '1-1.1.2' > /sys/bus/usb/drivers/usb/unbind
+echo '1-1.3' > /sys/bus/usb/drivers/usb/unbind
 sleep 2.5
-echo '1-1.1.2' > /sys/bus/usb/drivers/usb/unbind
+echo '1-1.3' > /sys/bus/usb/drivers/usb/unbind
 echo "Done"
 
